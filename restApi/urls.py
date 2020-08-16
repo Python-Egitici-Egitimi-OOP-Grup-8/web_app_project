c10984c7f72bfd06cf5489ee7767b75953826e26
sahinmansuroglu <sahinmansur@hotmail.com>
2020-08-16 10:25:21 +0300

    Login, signup ve Ogrenci Ekleme rest apileri kodlandı

--- restApi/urls.py
+++ restApi/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
 
     #todos
     path('',views.restapideneme),
-    
+    path('login',views.login),
+    path('signup',views.signup),
+    path('ogrencicevap',views.OgrenciCevapList.as_view()),
+    path('ogrencicevapekle',views.OgrenciCevapEkle.as_view()),
 ]
