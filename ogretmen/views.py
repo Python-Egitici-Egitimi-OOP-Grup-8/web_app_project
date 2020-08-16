957b5593f2a1ff3708e9fb07dcf04076939fd2b7
İbrahim KILIÇ <ibrahimkilic85@gmail.com>
2020-08-16 16:28:53 +0300

    öğretmen bölümü temel sayfa yapısı oluşturuldu.

--- ogretmen/views.py
+++ ogretmen/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def ogretmendeneme(request):
-    return render(request, 'ogretmen/ogretmendeneme.html')
+def ogretmenIndex(request):
+    return render(request, 'ogretmen/ogretmenIndex.html')
+
+def sinavlarim(request):
+    return render(request, 'ogretmen/sinavlarim.html')
+
+def profile(request):
+    return render(request, 'ogretmen/profile.html')
+
+def tokenAl(request):
+    return render(request, 'ogretmen/tokenAl.html')
