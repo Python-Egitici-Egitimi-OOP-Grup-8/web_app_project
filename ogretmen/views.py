3effc9e3733c7bd54c05e416e9ba93acc152c1a9
sahinmansuroglu <sahinmansur@hotmail.com>
2020-08-16 17:34:22 +0300

    Rest Api Test Edildi

--- ogretmen/views.py
+++ ogretmen/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def ogretmenIndex(request):
-    return render(request, 'ogretmen/ogretmenIndex.html')
-
-def sinavlarim(request):
-    return render(request, 'ogretmen/sinavlarim.html')
-
-def profile(request):
-    return render(request, 'ogretmen/profile.html')
-
-def tokenAl(request):
-    return render(request, 'ogretmen/tokenAl.html')
+def ogretmendeneme(request):
+    return render(request, 'ogretmen/ogretmendeneme.html')
