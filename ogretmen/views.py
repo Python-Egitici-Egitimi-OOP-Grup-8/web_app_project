86b98bb5f759e6b9a0a642ecd59bbaf72f4bb679
İbrahim KILIÇ <ibrahimkilic85@gmail.com>
2020-08-18 18:22:30 +0300

    açıklama satırları eklendi.

--- ogretmen/views.py
+++ ogretmen/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import update_session_auth_hash
-from django.shortcuts import render, redirect, get_object_or_404  # view function ile return işlemi esnasında kullanılacak fonksiyonlar
+from django.shortcuts import render, redirect, get_object_or_404  
 from oturum.models import *
 from .forms import *
 from django.contrib.auth.forms import PasswordChangeForm
@@ -7,12 +7,12 @@ from django.http import HttpResponse
 from rest_framework.authtoken.models import Token
 
 
-def ogretmenIndex(request): #öğretmen ana sayfa viewi
+def ogretmenIndex(request):
     return render(request, 'ogretmen/ogretmenIndex.html')
 
-def sinavlarim(request): #öğretmen sınavlarım bölümü viewi
-    sinavlar = Sinav.objects.filter(user=request.user)
-    if sinavlar:
+def sinavlarim(request):
+    sinavlar = Sinav.objects.filter(user=request.user) #aktif kullanıcıya ait sınav bilgileri sorgulanıyor.
+    if sinavlar: #sorgu başarılı ise sinavdolu değeri ile birlikte sözlük oluşturuluyor.
         context = {
             'sinavlar': sinavlar,
             'bosMu': False,
@@ -25,14 +25,14 @@ def sinavlarim(request): #öğretmen sınavlarım bölümü viewi
     return render(request, 'ogretmen/sinavlarim.html', context)
 
 def soruKazanim(request,pk):
-    sorusay = Sinav.objects.get(id=pk).sorusayisi
+    sinav = Sinav.objects.get(id=pk)
     kazanim = SoruKazanim.objects.get(sinav_id=pk)
     form = KazanimSoru(instance=kazanim)
     context = {
-        'form':form,
-        'say':sorusay
+        'form': form,
+        'say': sinav.sorusayisi
     }
-    if request.method == 'POST':
+    if request.method == 'POST': #sayfaya form üzerinden gelinmişse.
         form = KazanimSoru(request.POST, instance=kazanim)
         if form.is_valid():
             form.save()
@@ -41,16 +41,16 @@ def soruKazanim(request,pk):
 
 def soruPuan(request,pk):
     sorusay = Sinav.objects.get(id=pk).sorusayisi
-    puan = SoruPuanlama.objects.get(sinav_id=pk)
-    form = SoruPuanForm(instance= puan)
+    puan = SoruPuanlama.objects.get(sinav_id=pk) #Seçilen sınava ait puan değerleri getiriliyor.
+    form = SoruPuanForm(instance = puan) #Kullanıcıya görüntülenmek üzere puan değerleri ile form örneği oluşturuluyor.
     context = {
         'form':form,
         'say':sorusay
     }
-    if request.method == 'POST':
-        form = SoruPuanForm(request.POST, instance=puan)
+    if request.method == 'POST': #sayfaya form üzerinden gelinmişse.
+        form = SoruPuanForm(request.POST, instance=puan) #Formdan gelen soru puanları işlenmek üzere SoruPuanForm örneği oluşturuluyor.
         if form.is_valid():
-            form.save()
+            form.save() #yeni puan değerleri işleniyor.
             return redirect('/ogretmen/sinavlarim')
     return render(request, 'ogretmen/sorupuan.html', context)
 
@@ -62,66 +62,64 @@ def sinavSil(request,pk):
     context = {'item': sinav} #link üzerinden gelmişse silme işlemi onayı için yönlendir.
     return render(request, 'ogretmen/sinavSil.html', context)
 
-def profil(request): #öğretmen profil bölümü viewi
-    user = User.objects.get(id=request.user.id)
-    context = {
+def profil(request):
+    user = User.objects.get(id=request.user.id) #profil sayfasında görüntülenecek kullanıcıya bilgiler sorgulanıyor
+    context = { #görüntülnecek bilgiler sözlüğe ekleniyor
         'adi' : user.first_name,
         'soyadi' : user.last_name,
         'email' : user.email,
     }
-    # form = Profilim(instance=user)
-    # context = {'form': form}
     return render(request, 'ogretmen/profil.html',context)
 
 def updateProfil(request):
     user = User.objects.get(id=request.user.id)
-    form = Profilim(instance=user)
+    form = Profilim(instance=user) #user nesnesine ait profilim formunu oluştur.
     context = {'form': form}
-    if request.method == 'POST':
-        form = Profilim(request.POST, instance=user)
+    if request.method == 'POST': #sayfaya form üzerinden gelinmişse.
+        form = Profilim(request.POST, instance=user) #user nesnesine ait profilim formunu gelen veri ile oluştur.
         if form.is_valid():
-            form.save()
+            form.save() #formu kaydet ve veri tabanına işle. commit varsayılan değeri True
             return redirect('/ogretmen/profilim')
     return render(request, 'ogretmen/profilGuncelle.html', context)
 
 def parolaDegistir(request):
-    form = PasswordChangeForm(request.user)
+    form = PasswordChangeForm(request.user) #Belirtilen kullanıcıya ait parola değiştirme formu örnekleniyor.
     context = { 'form':form }
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user,request.POST or None)
+    if request.method == 'POST': #sayfaya form üzerinden gelinmişse.
+        form = PasswordChangeForm(request.user,request.POST) #elirtilen kullanıcıya ait parola değiştirme formunu yeni gelen veri ile örnekle
         if form.is_valid():
-            user = form.save()
+            user = form.save() #formu kaydet ve veri tabanına işle. commit varsayılan değeri True
             update_session_auth_hash(request, user) # Sistem atmasın diye yeni parola bilgileri ile oturumu güncelleniyor.
             return redirect('/ogretmen/profilim')
     return render(request, 'ogretmen/paroladegistir.html', context)
 
 def sinavEkle(request):
-    form = SinavEkle
-    context = {'form': form}
-    if request.method == 'POST':
+    form = SinavEkle  # SinavEkle django formunu örnekle
+    context = {'form': form} #template e gönderilmek üzere sözlüğe ekle.
+    if request.method == 'POST': #sayfaya form üzerinden gelinmişse.
         form =SinavEkle(request.POST)
         if form.is_valid():
-            sinav = form.save(commit=False)
-            sinav.user_id = request.user.id
-            sinav.save()
-            kazanim = SoruKazanim(sinav_id=sinav.id)
-            kazanim.save()
-            puan = SoruPuanlama(sinav_id=sinav.id)
-            puan.save()
+            sinav = form.save(commit=False) # formu kaydet fakat veri tabanına işleme.Bazı alanları düzenlemek üsere beklet.
+            sinav.user_id = request.user.id #userid alanını şu anki kullanıcı id si olarak ayarla.
+            sinav.save() #formu kaydet ve veri tabanına işle. commit varsayılan değeri True
+            kazanim = SoruKazanim(sinav_id=sinav.id) #SoruKazanim modelinde belirtilen sınava ait boş kayıt oluştur.
+            kazanim.save() #işle
+            puan = SoruPuanlama(sinav_id=sinav.id) #SoruPuanlama modelinde belirtilen sınava ait boş kayıt oluştur.
+            puan.save() #işle
             return redirect('/ogretmen/sinavlarim')
     return render(request, 'ogretmen/sinavEkle.html', context)
 
 def cevaplariEkle(request,pk):
-    sinav = Sinav.objects.get(id=pk)
-    form = CevapEkle(instance=sinav)
+    sinav = Sinav.objects.get(id=pk) #seçilen id değerine sahip sınav bilgileri getiriliyor.
+    form = CevapEkle(instance=sinav) #üzerinde işlem yapılacak sınav örneği için doğru cevap ekleme/değiştirme formu oluşturuluyor. Varsa değerler getiriliyor.
     context = {
         'form': form,
         'sorusay': sinav.sorusayisi,
     }
-    if request.method == 'POST':
-        form = CevapEkle(request.POST,instance=sinav)
+    if request.method == 'POST': #sayfaya form üzerinden gelinmişse.
+        form = CevapEkle(request.POST,instance=sinav) #yeni doğru cevap değerleri ile birlikte işlenmek üzere form oluşturuluyor.
         if form.is_valid():
-            form.save(commit=True)
+            form.save(commit=True) #veri tabanına işleniyor.
             return redirect('/ogretmen/sinavlarim')
     return render(request,'ogretmen/dogrucevap.html',context)
 
@@ -141,7 +139,7 @@ def raporAl(request,pk):
     return render(request,'ogretmen/rapor.html', context)
 
 def tokenAl(request):
-    token, created = Token.objects.get_or_create(user_id=request.user.id)
+    token, created = Token.objects.get_or_create(user_id=request.user.id) #varsa getir yoksa yeni token oluştur.
     context = {'anahtar':token}
     return render(request, 'ogretmen/tokenAl.html',context)
 
