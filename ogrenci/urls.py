82bb34fe1eb1e2668a91a407d80a9264970ff63b
toncel <68423309+toncel@users.noreply.github.com>
2020-08-27 11:26:39 +0300

    öğrenci kısmı düzenlemeleri
    öğrenci kısmı düzenlemeleri

--- ogrenci/urls.py
+++ ogrenci/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from .views import PDFOlustur
 
 urlpatterns = [
-    
-    path('',views.ogrencideneme),
-    
+    path('',views.ogretmenIndex, name='ogretmen'),
+    path('sinavlarim',views.sinavlarim, name='sinavlarim'),
+    path('profilim',views.profil, name='profilim'),
+    path('raporal/<str:pk>', views.raporAl, name='raporal'),
+    path('paroladegis', views.parolaDegistir, name='paroladegis'),
+    path('profilguncelle', views.updateProfil, name='profilguncelle'),
+    path('pdfal/<str:pk>', PDFOlustur.as_view(), name='pdfal'),
 ]
