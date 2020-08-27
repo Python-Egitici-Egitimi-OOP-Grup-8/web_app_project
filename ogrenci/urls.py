e5d1afdb3cdb4ed5f1b8f434f344d217d37f59a2
İbrahim KILIÇ <ibrahimkilic85@gmail.com>
2020-08-27 21:27:05 +0300

    ufak hata düzeltmeleri ve test için kullanıcı rol değişimi.

--- ogrenci/urls.py
+++ ogrenci/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 from . import views
-from .views import PDFOlustur
 
 urlpatterns = [
     path('',views.ogrenciIndex, name='ogrhome'),
     path('sinavlarim',views.sinavlarim, name='ogrsinavlarim'),
     path('profilim',views.profil, name='ogrprofilim'),
-    path('raporal/<str:pk>', views.raporAl, name='raporal'),
-    path('paroladegis', views.parolaDegistir, name='paroladegis'),
-    path('profilguncelle', views.updateProfil, name='profilguncelle'),
-    path('pdfal/<str:pk>', PDFOlustur.as_view(), name='pdfal'),
 ]
